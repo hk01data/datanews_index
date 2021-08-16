039f1fd103c67268d2c22f28c5fadfc8c2643cf2
hk01data <21330351+hk01data@users.noreply.github.com>
2021-08-16 15:12:38 +0800

    GitHub Actions 3

--- scripts/app1_getjson.js
+++ scripts/app1_getjson.js
@@ -57,31 +57,8 @@ function loadJSON(cookieString) {
 }
 
 
-function convertDate () {
-  let res = fs.readFileSync(`public/list.json`, {encoding: 'utf-8'})
-  let ajson = JSON.parse(res)
-  let dateNowObj = new Date()
-  let newTs = []
-
-  if (ajson['projects_and_tags']) {
-    newTs = ajson['projects_and_tags'].map(o => {
-      return {
-        ...o,
-        timestamp_str: chrono.parseDate(o['timestamp_str'], dateNowObj)
-      }
-    })
-  }
-
-  let bjson = JSON.parse(res)
-  bjson['projects_and_tags'] = newTs
-
-  fs.writeFileSync(`public/list_ts.json`, JSON.stringify(bjson), {encoding: 'utf-8'})
-}
-
-
 function main() {
   util.doLogin(loadJSON)
-  convertDate()
 }
 
 main()
