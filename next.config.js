1d731098477c033b7f8961492d2c4cde476ef4f4
hk01data <21330351+hk01data@users.noreply.github.com>
2021-08-16 16:22:20 +0800

    GitHub Action 15

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
 module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      basePath: '/',
+    }
+  }
+
   /**
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
     /* config options here */
+    basePath: '/datanews_index',
     images: {
       loader: 'akamai'
     }
